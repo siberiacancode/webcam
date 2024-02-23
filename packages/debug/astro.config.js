9a9f108d68c3fac1c10c1a909b2cf3dc7cdd81df
debabin <dmitriybabinup@gmail.com>
2024-02-23 18:21:58 +0700

    🧊 return to dist

--- packages/debug/astro.config.js
+++ packages/debug/astro.config.js
@@ -1,13 +1,14 @@
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
-import vercelServerless from '@astrojs/vercel/serverless';
+// import vercelServerless from '@astrojs/vercel/serverless';
+
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: vercelServerless({
-    webAnalytics: { enabled: true }
-  }),
+  // output: 'server',
+  // adapter: vercelServerless({
+  //   webAnalytics: { enabled: true }
+  // }),
   integrations: [
     tailwind({
       applyBaseStyles: false
