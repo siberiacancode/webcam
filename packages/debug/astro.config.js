f45739a9d1187b590ba92afb6ef518a391cd8d01
debabin <dmitriybabinup@gmail.com>
2024-02-23 16:45:36 +0700

    🧊 add vercel analytics

--- packages/debug/astro.config.js
+++ packages/debug/astro.config.js
@@ -1,6 +1,7 @@
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,11 +12,14 @@ export default defineConfig({
       prefixDefaultLocale: true
     }
   },
-
   integrations: [
     tailwind({
       applyBaseStyles: false
     }),
     react()
-  ]
+  ],
+  output: 'server',
+  adapter: vercel({
+    webAnalytics: { enabled: true }
+  })
 });
